80076173d11b2285ee6ec025ce8c9d51dc635ed0
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-01-22 11:48:14 -0500

    test admins were not running lolz

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -7,7 +7,7 @@ class AdminSiteTests(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.admin_user = get_user_model.objects.create_superuser(
+        self.admin_user = get_user_model().objects.create_superuser(
             email="admin@email.com",
             password="pass123"
         )
@@ -20,6 +20,12 @@ class AdminSiteTests(TestCase):
 
     def test_users_listed(self):
         url = reverse('admin:core_user_changelist')
-        res = self.Client.get(url)
+        res = self.client.get(url)
+
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    # def test_user_change_page(self):
+    #     url = reverse('admin:core_user_change', args=[self.user.id])
+    #     res = self.client.get(url)
+    #     self.assertEqual(res.status_code, 200)
