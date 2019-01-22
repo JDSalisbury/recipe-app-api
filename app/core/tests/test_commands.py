93e53406d1ecfb08a833db825e0a37ee5dc4cd33
JDSalisbury <Salisbury.jeffery@gmail.com>
2019-01-22 13:35:27 -0500

    setting up test to make a command to wait for db. Test Red

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,10 +1,20 @@
-# from unittest.mock import patch
-# from django.core.management import call_command
-# from django.db.utils import OperationalError
-# from django.test import TestCase
-#
-#
-# class CommandTests(TestCase):
-#
-#     def test_wait_for_db_ready(self):
-#         pass
+from unittest.mock import patch
+from django.core.management import call_command
+from django.db.utils import OperationalError
+from django.test import TestCase
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True #mocked
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True) #mocking the actual time.sleep
+    def test_wait_for_db(self, ts):
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OpperationError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
